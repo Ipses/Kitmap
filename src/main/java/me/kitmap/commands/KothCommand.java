fe106f8171f42873ef973ecf8c957287d0489a30
BuildTools <unconfigured@null.spigotmc.org>
2020-06-24 04:47:20 +0900

    comment #4: added return statement at the beginning for koth command if the condition is false

--- src/main/java/me/kitmap/commands/KothCommand.java
+++ src/main/java/me/kitmap/commands/KothCommand.java
@@ -23,53 +23,54 @@ public class KothCommand implements CommandExecutor {
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String arg1, String[] args ) {
-        if(cmd.getName().equalsIgnoreCase("koth") && sender instanceof Player) {
-            Player player = (Player) sender;
-            if(args.length == 0){
-                player.sendMessage("/koth list: view all koths");
-                player.sendMessage("/koth start <name>: start koth");
-                player.sendMessage("/koth end <name> end koth");
-                return true;
-            }
-            if(args[0].equalsIgnoreCase("list")){
-                player.sendMessage("Koth List");
-                player.sendMessage("Eillom");
-                return true;
-            }
-            if(args[0].equalsIgnoreCase("start")){
-                if(args.length == 1){
-                    player.sendMessage("enter a valid koth name in /koth list");
-                    return true;
-                }
-                if(plugin.getKothManager().isRunning()){
-                    player.sendMessage("There's already a KOTH running");
-                    return true;
-                }
-                if(args[1].equalsIgnoreCase("Eillom")){
-                    double eillomMinX = Double.parseDouble(configManager.getCoords().getString("Eillom.minX"));
-                    double eillomMaxX = Double.parseDouble(configManager.getCoords().getString("Eillom.maxX"));
-                    double eillomMinZ = Double.parseDouble(configManager.getCoords().getString("Eillom.minZ"));
-                    double eillomMaxZ = Double.parseDouble(configManager.getCoords().getString("Eillom.maxZ"));
-                    Koth eillom = new Koth("Eillom", new Location(player.getWorld(),
-                            eillomMinX, 1, eillomMinZ),
-                            new Location(player.getWorld(), eillomMaxX, 1, 411), TimeUnit.SECONDS.toMillis(10));
-                    this.plugin.getKothManager().addKoth(eillom);
-                    this.plugin.getKothManager().start();
-                    return true;
-                }
-                // add more koth here
+        if (!(cmd.getName().equalsIgnoreCase("koth") && sender instanceof Player)) {
+            return false;
+        }
+        Player player = (Player) sender;
+        if(args.length == 0){
+            player.sendMessage("/koth list: view all koths");
+            player.sendMessage("/koth start <name>: start koth");
+            player.sendMessage("/koth end <name> end koth");
+            return true;
+        }
+        if(args[0].equalsIgnoreCase("list")){
+            player.sendMessage("Koth List");
+            player.sendMessage("Eillom");
+            return true;
+        }
+        if(args[0].equalsIgnoreCase("start")){
+            if(args.length == 1){
                 player.sendMessage("enter a valid koth name in /koth list");
                 return true;
             }
-
-            if(args[0].equalsIgnoreCase("end")){
-                if(!this.plugin.getKothManager().isRunning()){
-                    player.sendMessage("There's no koth running");
-                    return false;
-                }
-                plugin.getKothManager().end();
+            if(plugin.getKothManager().isRunning()){
+                player.sendMessage("There's already a KOTH running");
                 return true;
             }
+            if(args[1].equalsIgnoreCase("Eillom")){
+                double eillomMinX = Double.parseDouble(configManager.getCoords().getString("Eillom.minX"));
+                double eillomMaxX = Double.parseDouble(configManager.getCoords().getString("Eillom.maxX"));
+                double eillomMinZ = Double.parseDouble(configManager.getCoords().getString("Eillom.minZ"));
+                double eillomMaxZ = Double.parseDouble(configManager.getCoords().getString("Eillom.maxZ"));
+                Koth eillom = new Koth("Eillom", new Location(player.getWorld(),
+                        eillomMinX, 1, eillomMinZ),
+                        new Location(player.getWorld(), eillomMaxX, 1, 411), TimeUnit.SECONDS.toMillis(10));
+                this.plugin.getKothManager().addKoth(eillom);
+                this.plugin.getKothManager().start();
+                return true;
+            }
+            // add more koth here
+            player.sendMessage("enter a valid koth name in /koth list");
+            return true;
+        }
+
+        if(args[0].equalsIgnoreCase("end")){
+            if(!this.plugin.getKothManager().isRunning()){
+                player.sendMessage("There's no koth running");
+                return false;
+            }
+            plugin.getKothManager().end();
+            return true;
         }
         return false;
     }
