f6ad850abefe55daeed254dd7ba0f038f74ef616
BuildTools <unconfigured@null.spigotmc.org>
2020-06-26 05:43:28 +0900

    changed if to switch

--- src/main/java/me/kitmap/commands/KothCommand.java
+++ src/main/java/me/kitmap/commands/KothCommand.java
@@ -33,45 +33,46 @@ public class KothCommand implements CommandExecutor {
             player.sendMessage("/koth end <name> end koth");
             return true;
         }
-        if(args[0].equalsIgnoreCase("list")){
-            player.sendMessage("Koth List");
-            player.sendMessage("Eillom");
-            return true;
-        }
-        if(args[0].equalsIgnoreCase("start")){
-            if(args.length == 1){
+        switch(args[0].toLowerCase()){
+            default:
+                return false;
+            case "list":
+                player.sendMessage("Koth List");
+                player.sendMessage("Eillom");
+                return true;
+            case "start":
+                if(args.length == 1){
+                    player.sendMessage("enter a valid koth name in /koth list");
+                    return true;
+                }
+                if(plugin.getKothManager().isRunning()){
+                    player.sendMessage("There's already a KOTH running");
+                    return true;
+                }
+                if(args[1].equalsIgnoreCase("Eillom")){
+                    double eillomMinX = Double.parseDouble(configManager.getCoords().getString("Eillom.minX"));
+                    double eillomMaxX = Double.parseDouble(configManager.getCoords().getString("Eillom.maxX"));
+                    double eillomMinZ = Double.parseDouble(configManager.getCoords().getString("Eillom.minZ"));
+                    double eillomMaxZ = Double.parseDouble(configManager.getCoords().getString("Eillom.maxZ"));
+                    Koth eillom = new Koth("Eillom", new Location(player.getWorld(),
+                            eillomMinX, 1, eillomMinZ),
+                            new Location(player.getWorld(), eillomMaxX, 1, 411), TimeUnit.SECONDS.toMillis(10));
+                    this.plugin.getKothManager().addKoth(eillom);
+                    this.plugin.getKothManager().start();
+                    return true;
+                }
+                // add more koth here
                 player.sendMessage("enter a valid koth name in /koth list");
                 return true;
-            }
-            if(plugin.getKothManager().isRunning()){
-                player.sendMessage("There's already a KOTH running");
+            case "end":
+                if(!this.plugin.getKothManager().isRunning()){
+                    player.sendMessage("There's no koth running");
+                    return false;
+                }
+                plugin.getKothManager().end();
                 return true;
             }
-            if(args[1].equalsIgnoreCase("Eillom")){
-                double eillomMinX = Double.parseDouble(configManager.getCoords().getString("Eillom.minX"));
-                double eillomMaxX = Double.parseDouble(configManager.getCoords().getString("Eillom.maxX"));
-                double eillomMinZ = Double.parseDouble(configManager.getCoords().getString("Eillom.minZ"));
-                double eillomMaxZ = Double.parseDouble(configManager.getCoords().getString("Eillom.maxZ"));
-                Koth eillom = new Koth("Eillom", new Location(player.getWorld(),
-                        eillomMinX, 1, eillomMinZ),
-                        new Location(player.getWorld(), eillomMaxX, 1, 411), TimeUnit.SECONDS.toMillis(10));
-                this.plugin.getKothManager().addKoth(eillom);
-                this.plugin.getKothManager().start();
-                return true;
-            }
-            // add more koth here
-            player.sendMessage("enter a valid koth name in /koth list");
-            return true;
         }
-
-        if(args[0].equalsIgnoreCase("end")){
-            if(!this.plugin.getKothManager().isRunning()){
-                player.sendMessage("There's no koth running");
-                return false;
-            }
-            plugin.getKothManager().end();
-            return true;
-        }
-        return false;
     }
-}
+
+
